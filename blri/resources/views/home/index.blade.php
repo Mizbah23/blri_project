a950798748a5271bbc78b06b470434f0b4a840be
Mizbah23 <mizbahmed161@gmail.com>
2019-08-03 12:30:50 +0600

    adding few

--- blri/resources/views/home/index.blade.php
+++ blri/resources/views/home/index.blade.php
@@ -133,13 +133,13 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
               </li>
               <li class="treeview">
                 <a href="#">
-                <i class="fa fa-laptop"></i>
+                <i class="fa fa-folder"></i>
                 <span>Components</span>
                 <i class="fa fa-angle-left pull-right"></i>
                 </a>
                 <ul class="treeview-menu">
-                  <li><a href="grids.html"><i class="fa fa-angle-right"></i> Grids</a></li>
-                  <li><a href="media.html"><i class="fa fa-angle-right"></i> Media Css</a></li>
+                  <li><a href="grids.html"><i class="fa fa-circle"></i> Grids</a></li>
+                  <li><a href="media.html"><i class="fa fa-circle"></i> Media Css</a></li>
                 </ul>
               </li>
               <li class="treeview">
@@ -157,10 +157,10 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
                 <i class="fa fa-angle-left pull-right"></i>
                 </a>
                 <ul class="treeview-menu">
-                  <li><a href="general.html"><i class="fa fa-angle-right"></i> General</a></li>
-                  <li><a href="icons.html"><i class="fa fa-angle-right"></i> Icons</a></li>
-                  <li><a href="buttons.html"><i class="fa fa-angle-right"></i> Buttons</a></li>
-                  <li><a href="typography.html"><i class="fa fa-angle-right"></i> Typography</a></li>
+                  <li><a href="general.html"><i class="fa fa-circle"></i> General</a></li>
+                  <li><a href="icons.html"><i class="fa fa-circle"></i> Icons</a></li>
+                  <li><a href="buttons.html"><i class="fa fa-circle"></i> Buttons</a></li>
+                  <li><a href="typography.html"><i class="fa fa-circle"></i> Typography</a></li>
                 </ul>
               </li>
               <li>
@@ -175,8 +175,8 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
                 <i class="fa fa-angle-left pull-right"></i>
                 </a>
                 <ul class="treeview-menu">
-                  <li><a href="forms.html"><i class="fa fa-angle-right"></i> General Forms</a></li>
-                  <li><a href="validation.html"><i class="fa fa-angle-right"></i> Form Validations</a></li>
+                  <li><a href="forms.html"><i class="fa fa-circle"></i> General Forms</a></li>
+                  <li><a href="validation.html"><i class="fa fa-circle"></i> Form Validations</a></li>
                 </ul>
               </li>
               <li class="treeview">
@@ -193,8 +193,8 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
                 <i class="fa fa-envelope"></i> <span>Mailbox </span>
                 <i class="fa fa-angle-left pull-right"></i><small class="label pull-right label-info1">08</small><span class="label label-primary1 pull-right">02</span></a>
                 <ul class="treeview-menu">
-                  <li><a href="inbox.html"><i class="fa fa-angle-right"></i> Mail Inbox </a></li>
-                  <li><a href="compose.html"><i class="fa fa-angle-right"></i> Compose Mail </a></li>
+                  <li><a href="inbox.html"><i class="fa fa-circle"></i> Mail Inbox </a></li>
+                  <li><a href="compose.html"><i class="fa fa-circle"></i> Compose Mail </a></li>
                 </ul>
               </li>
               <li class="treeview">
@@ -203,11 +203,11 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
                 <i class="fa fa-angle-left pull-right"></i>
                 </a>
                 <ul class="treeview-menu">
-                  <li><a href="login.html"><i class="fa fa-angle-right"></i> Login</a></li>
-                  <li><a href="signup.html"><i class="fa fa-angle-right"></i> Register</a></li>
-                  <li><a href="404.html"><i class="fa fa-angle-right"></i> 404 Error</a></li>
-                  <li><a href="500.html"><i class="fa fa-angle-right"></i> 500 Error</a></li>
-                  <li><a href="blank-page.html"><i class="fa fa-angle-right"></i> Blank Page</a></li>
+                  <li><a href="login.html"><i class="fa fa-circle"></i> Login</a></li>
+                  <li><a href="signup.html"><i class="fa fa-circle"></i> Register</a></li>
+                  <li><a href="404.html"><i class="fa fa-circle"></i> 404 Error</a></li>
+                  <li><a href="500.html"><i class="fa fa-circle"></i> 500 Error</a></li>
+                  <li><a href="blank-page.html"><i class="fa fa-circle"></i> Blank Page</a></li>
                 </ul>
               </li>
               <li class="header">LABELS</li>
