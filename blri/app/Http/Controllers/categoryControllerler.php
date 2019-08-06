a96431a0102d781dc0b46514e65d64b8764fcdcf
Mizbah Uddin Ahmed <mizbahmed161@gmail.com>
2019-08-07 03:12:01 +0600

    update

--- blri/app/Http/Controllers/categoryControllerler.php
+++ blri/app/Http/Controllers/categoryControllerler.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\setuptype;
 
 class categoryControllerler extends Controller
 {
-    //
+   public function index(){
+   $setuptypes= setuptype::all();
+   return view('setup.designation')->with('setuptypes',$setuptypes);
+       }
 }
