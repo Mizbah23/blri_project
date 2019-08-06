a63ddd48c0be79dbd99e59ba9b8fd6c2a10466b0
Mizbah Uddin Ahmed <mizbahmed161@gmail.com>
2019-08-07 03:39:33 +0600

    Update division.blade.php

--- blri/resources/views/setup/division.blade.php
+++ blri/resources/views/setup/division.blade.php
@@ -387,7 +387,7 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
                         <input class="sb-search-input input__field--madoka" placeholder="Search..." type="search" id="input-31" />
                         <label class="input__label" for="input-31">
                             <svg class="graphic" width="100%" height="100%" viewBox="0 0 404 77" preserveAspectRatio="none">
-                                <path d="m0,0l404,0l0,77l-404,0l0,-77z"/>
+                                <path d="/m0,0l404,0l0,77l-404,0l0,-77z"/>
                             </svg>
                         </label>
                     </form>
@@ -398,7 +398,7 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
                         <li class="dropdown profile_details_drop">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
                                 <div class="profile_img">   
-                                    <span class="prfil-img"><img src="images/2.jpg" alt=""> </span> 
+                                    <span class="prfil-img"><img src="/images/2.jpg" alt=""> </span> 
                                     <div class="user-name">
                                         <p>Admin Name</p>
                                         <span>Administrator</span>
