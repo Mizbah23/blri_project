622fec678f71d3bcf4a9a3dfdb2e455a82fa0d4a
jotonsd <53141421+jotonsd@users.noreply.github.com>
2019-08-18 15:58:01 +0600

    logo attachment (18.08.19)

--- blri/resources/views/home/index.blade.php
+++ blri/resources/views/home/index.blade.php
@@ -3,6 +3,7 @@
 <html>
 <head>
 <title> Home</title>
+<link rel="icon" type="image/png" href="/images/logo.png" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Glance Design Dashboard Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
@@ -116,8 +117,16 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             </button>
-            <h1><a class="navbar-brand" href="index.html"><span class="fa fa-area-chart"></span> BLRI<span class="dashboard_text">Design dashboard</span></a></h1>
-          </div>
+
+            <div style="margin-top: 10px">
+              <div class="col-lg-3 row mt-5">
+                <img style="height: 50px; width: 50px;" src="/images/logo.png" alt="">
+              </div>
+              <div class="col-lg-9">
+                 <h1 style="margin-left: -25px"><a class="navbar-brand" href="index.html">  BLRI<span class="dashboard_text" style="margin-left: -35px">Design dashboard</span></a></h1>
+              </div>
+             </div>
+            </div>
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="sidebar-menu">
               <li class="header">MAIN NAVIGATION</li>
@@ -219,186 +228,21 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
     </div>
         <!--left-fixed -navigation-->
         
-        <!-- header-starts -->
+         <!-- header-starts -->
         <div class="sticky-header header-section ">
             <div class="header-left">
                 <!--toggle button start-->
                 <button id="showLeftPush"><i class="fa fa-bars"></i></button>
                 <!--toggle button end-->
-                <div class="profile_details_left"><!--notifications of menu start -->
-                    <ul class="nofitications-dropdown">
-                        <li class="dropdown head-dpdn">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-envelope"></i><span class="badge">4</span></a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <div class="notification_header">
-                                        <h3>You have 3 new messages</h3>
-                                    </div>
-                                </li>
-                                <li><a href="#">
-                                   <div class="user_img"><img src="images/1.jpg" alt=""></div>
-                                   <div class="notification_desc">
-                                    <p>Lorem ipsum dolor amet</p>
-                                    <p><span>1 hour ago</span></p>
-                                    </div>
-                                   <div class="clearfix"></div> 
-                                </a></li>
-                                <li class="odd"><a href="#">
-                                    <div class="user_img"><img src="images/4.jpg" alt=""></div>
-                                   <div class="notification_desc">
-                                    <p>Lorem ipsum dolor amet </p>
-                                    <p><span>1 hour ago</span></p>
-                                    </div>
-                                  <div class="clearfix"></div>  
-                                </a></li>
-                                <li><a href="#">
-                                   <div class="user_img"><img src="images/3.jpg" alt=""></div>
-                                   <div class="notification_desc">
-                                    <p>Lorem ipsum dolor amet </p>
-                                    <p><span>1 hour ago</span></p>
-                                    </div>
-                                   <div class="clearfix"></div> 
-                                </a></li>
-                                <li><a href="#">
-                                   <div class="user_img"><img src="images/2.jpg" alt=""></div>
-                                   <div class="notification_desc">
-                                    <p>Lorem ipsum dolor amet </p>
-                                    <p><span>1 hour ago</span></p>
-                                    </div>
-                                   <div class="clearfix"></div> 
-                                </a></li>
-                                <li>
-                                    <div class="notification_bottom">
-                                        <a href="#">See all messages</a>
-                                    </div> 
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="dropdown head-dpdn">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-bell"></i><span class="badge blue">4</span></a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <div class="notification_header">
-                                        <h3>You have 3 new notification</h3>
-                                    </div>
-                                </li>
-                                <li><a href="#">
-                                    <div class="user_img"><img src="images/4.jpg" alt=""></div>
-                                   <div class="notification_desc">
-                                    <p>Lorem ipsum dolor amet</p>
-                                    <p><span>1 hour ago</span></p>
-                                    </div>
-                                  <div class="clearfix"></div>  
-                                 </a></li>
-                                 <li class="odd"><a href="#">
-                                    <div class="user_img"><img src="images/1.jpg" alt=""></div>
-                                   <div class="notification_desc">
-                                    <p>Lorem ipsum dolor amet </p>
-                                    <p><span>1 hour ago</span></p>
-                                    </div>
-                                   <div class="clearfix"></div> 
-                                 </a></li>
-                                 <li><a href="#">
-                                    <div class="user_img"><img src="images/3.jpg" alt=""></div>
-                                   <div class="notification_desc">
-                                    <p>Lorem ipsum dolor amet </p>
-                                    <p><span>1 hour ago</span></p>
-                                    </div>
-                                   <div class="clearfix"></div> 
-                                 </a></li>
-                                <li><a href="#">
-                                   <div class="user_img"><img src="images/2.jpg" alt=""></div>
-                                   <div class="notification_desc">
-                                    <p>Lorem ipsum dolor amet </p>
-                                    <p><span>1 hour ago</span></p>
-                                    </div>
-                                   <div class="clearfix"></div> 
-                                </a></li>
-                                 <li>
-                                    <div class="notification_bottom">
-                                        <a href="#">See all notifications</a>
-                                    </div> 
-                                </li>
-                            </ul>
-                        </li>   
-                        <li class="dropdown head-dpdn">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-tasks"></i><span class="badge blue1">8</span></a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <div class="notification_header">
-                                        <h3>You have 8 pending task</h3>
-                                    </div>
-                                </li>
-                                <li><a href="#">
-                                    <div class="task-info">
-                                        <span class="task-desc">Database update</span><span class="percentage">40%</span>
-                                        <div class="clearfix"></div>    
-                                    </div>
-                                    <div class="progress progress-striped active">
-                                        <div class="bar yellow" style="width:40%;"></div>
-                                    </div>
-                                </a></li>
-                                <li><a href="#">
-                                    <div class="task-info">
-                                        <span class="task-desc">Dashboard done</span><span class="percentage">90%</span>
-                                       <div class="clearfix"></div> 
-                                    </div>
-                                    <div class="progress progress-striped active">
-                                         <div class="bar green" style="width:90%;"></div>
-                                    </div>
-                                </a></li>
-                                <li><a href="#">
-                                    <div class="task-info">
-                                        <span class="task-desc">Mobile App</span><span class="percentage">33%</span>
-                                        <div class="clearfix"></div>    
-                                    </div>
-                                   <div class="progress progress-striped active">
-                                         <div class="bar red" style="width: 33%;"></div>
-                                    </div>
-                                </a></li>
-                                <li><a href="#">
-                                    <div class="task-info">
-                                        <span class="task-desc">Issues fixed</span><span class="percentage">80%</span>
-                                       <div class="clearfix"></div> 
-                                    </div>
-                                    <div class="progress progress-striped active">
-                                         <div class="bar  blue" style="width: 80%;"></div>
-                                    </div>
-                                </a></li>
-                                <li>
-                                    <div class="notification_bottom">
-                                        <a href="#">See all pending tasks</a>
-                                    </div> 
-                                </li>
-                            </ul>
-                        </li>   
-                    </ul>
-                    <div class="clearfix"> </div>
-                </div>
-                <!--notification menu end -->
                 <div class="clearfix"> </div>
             </div>
-            <div class="header-right">
-                
-                
-                <!--search-box-->
-                <div class="search-box">
-                    <form class="input">
-                        <input class="sb-search-input input__field--madoka" placeholder="Search..." type="search" id="input-31" />
-                        <label class="input__label" for="input-31">
-                            <svg class="graphic" width="100%" height="100%" viewBox="0 0 404 77" preserveAspectRatio="none">
-                                <path d="m0,0l404,0l0,77l-404,0l0,-77z"/>
-                            </svg>
-                        </label>
-                    </form>
-                </div><!--//end-search-box-->
-                
+            
                 <div class="profile_details">       
                     <ul>
                         <li class="dropdown profile_details_drop">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
                                 <div class="profile_img">   
-                                    <span class="prfil-img"><img src="images/2.jpg" alt=""> </span> 
+                                    <span class="prfil-img"><img src="/images/2.jpg" alt=""> </span> 
                                     <div class="user-name">
                                         <p>Admin Name</p>
                                         <span>Administrator</span>
@@ -417,11 +261,14 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
                         </li>
                     </ul>
                 </div>
+
+
                 <div class="clearfix"> </div>               
             </div>
             <div class="clearfix"> </div>   
         </div>
         <!-- //header-ends -->
+   
         <!-- main content start-->
                         <h1> THIS  IS DASHBOARD</h1>
     <!--footer-->
