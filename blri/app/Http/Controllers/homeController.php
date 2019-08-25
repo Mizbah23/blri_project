3209171893888f5c01d7d7422e8186605cb82d85
Joton Sutradhar <53141421+jotonsd@users.noreply.github.com>
2019-08-25 16:52:19 +0600

    update

--- blri/app/Http/Controllers/homeController.php
+++ blri/app/Http/Controllers/homeController.php
@@ -22,12 +22,13 @@ class homeController extends Controller
         $productreceivetypes=ProductReceiveType::all();
         $productdistributions=ProductDistribution::all();
         $adjustments=Adjustment::all();
-
-        //dd($sections[0]->division);
-        return view('home.index')->with('setuptypes',$setuptypes)->with('securitytypes',$securitytypes)->with('productreceivetypes',$productreceivetypes)->with('productdistributions',$productdistributions)->with('adjustments',$adjustments);
         $reportings=Reporting::all();
 
         //dd($sections[0]->division);
-        return view('home.index')->with('setuptypes',$setuptypes)->with('securitytypes',$securitytypes)->with('productreceivetypes',$productreceivetypes)->with('productdistributions',$productdistributions)->with('reportings',$reportings);
+        return view('home.index')->with('setuptypes',$setuptypes)->with('securitytypes',$securitytypes)->with('productreceivetypes',$productreceivetypes)->with('productdistributions',$productdistributions)->with('adjustments',$adjustments)->with('reportings',$reportings);
+        
+
+        //dd($sections[0]->division);
+        
     }
 }
