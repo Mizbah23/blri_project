c8de5bea7196e718d44b9a96db613d3fb4d94df4
Md-AshiqurRahman <53116858+Md-AshiqurRahman@users.noreply.github.com>
2019-08-25 20:53:53 +0600

    nl

--- blri/resources/views/home/index.blade.php
+++ blri/resources/views/home/index.blade.php
@@ -171,7 +171,7 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
               <li class="treeview">
                 <a href="#">
                 <i class="fa fa-laptop"></i>
-                <span>Product Recieve</span>
+                <span>Product Receive</span>
                 <i class="fa fa-angle-left pull-right"></i>
                 </a>
                 <ul class="treeview-menu">
