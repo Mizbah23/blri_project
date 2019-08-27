064b97c4216bf8b8b1411aec9026c096052b8615
Md-AshiqurRahman <53116858+Md-AshiqurRahman@users.noreply.github.com>
2019-08-27 14:17:37 +0600

    new page

--- blri/app/Http/Controllers/productreleaseController.php
+++ blri/app/Http/Controllers/productreleaseController.php
@@ -7,6 +7,10 @@ use App\setuptype;
 use App\SecurityType;
 use App\ProductReceiveType;
 use App\ProductDistribution;
+use App\Adjustment;
+use App\Reporting;
+
+
 
 class productreleaseController extends Controller
 {
@@ -16,8 +20,17 @@ class productreleaseController extends Controller
     $securitytypes= SecurityType::all();
     $productreceivetypes=ProductReceiveType::all();
     $productdistributions=ProductDistribution::all();
+    $adjustments=Adjustment::all();
+    $reportings=Reporting::all();
 
 
-    return view('product distribution.product release')->with('setuptypes',$setuptypes)->with('securitytypes',$securitytypes)->with('productreceivetypes',$productreceivetypes)->with('productdistributions',$productdistributions);
+
+    return view('product distribution.product release')
+                ->with('setuptypes',$setuptypes)
+                ->with('securitytypes',$securitytypes)
+                ->with('productreceivetypes',$productreceivetypes)
+                ->with('productdistributions',$productdistributions)
+                ->with('adjustments',$adjustments)
+                ->with('reportings',$reportings);
 	}
 }
