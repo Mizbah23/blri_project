08002ad41ff042cbf454cbf5a2646c5f1d3ef2fb
Mizbah23 <mizbahmed161@gmail.com>
2019-09-06 12:36:42 +0600

    Update create user.blade.php

--- blri/resources/views/security/create user.blade.php
+++ blri/resources/views/security/create user.blade.php
@@ -166,15 +166,15 @@ $( function() {
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             </button>
-
-            <div style="margin-top: 10px">
+              <h1><a class="navbar-brand" href="index.html"><span> <img style="height: 50px; width: 50px;" src="/images/logo.png" alt=""></span> BLRI</a></h1>
+           <!-- <div style="margin-top: 10px">
               <div class="col-lg-3">
                 <img style="height: 50px; width: 50px;" src="/images/logo.png" alt="">
               </div>
               <div class="col-lg-9">
                  <h1 style="margin-left: -25px"><a class="navbar-brand" href="index.html">  BLRI<span class="dashboard_text" style="margin-left: -35px">Design dashboard</span></a></h1>
               </div>
-             </div>
+             </div>-->
             </div>
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="sidebar-menu">
