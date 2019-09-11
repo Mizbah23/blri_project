43482785c14393359d8e859c94eada5b58fac5ad
IKTANIM <iktanim1995@gmail.com>
2019-09-11 19:20:12 +0600

    RequitionInfoController save error fix

--- blri/app/Http/Controllers/requisitioninfoController.php
+++ blri/app/Http/Controllers/requisitioninfoController.php
@@ -19,40 +19,37 @@ use App\RequisitionSave;
 
 class requisitioninfoController extends Controller
 {
-         public function index(Request $request)
+    public function index(Request $request)
     {
-    	if($request->session()->get('user')){
-        $setuptypes= setuptype::all();
-        $securitytypes=SecurityType::all();
-        $productreceivetypes=ProductReceiveType::all();
-        $productdistributions=ProductDistribution::all();
-        $adjustments=Adjustment::all();
-        $reportings=Reporting::all();
-        $employeeinformations=EmployeeInformation::all();
-        $requisitionlists=RequisitionList::all();
-        $products=ProductInfo::all();
-        $brands=Brand::all();
-        // dd($products->unique('productName'));
-        //dd($sections[0]->division);
-        return view('product receive.requisition info')->with('setuptypes',$setuptypes)
-                                     ->with('securitytypes',$securitytypes)
-                                     ->with('productreceivetypes',$productreceivetypes)
-                                     ->with('productdistributions',$productdistributions)
-                                     ->with('adjustments',$adjustments)
-                                     ->with('reportings',$reportings)
+        if ($request->session()->get('user')) {
+            $setuptypes= setuptype::all();
+            $securitytypes=SecurityType::all();
+            $productreceivetypes=ProductReceiveType::all();
+            $productdistributions=ProductDistribution::all();
+            $adjustments=Adjustment::all();
+            $reportings=Reporting::all();
+            $employeeinformations=EmployeeInformation::all();
+            $requisitionlists=RequisitionList::all();
+            $products=ProductInfo::all();
+            $brands=Brand::all();
+            // dd($products->unique('productName'));
+            //dd($sections[0]->division);
+            return view('product receive.requisition info')->with('setuptypes', $setuptypes)
+                                     ->with('securitytypes', $securitytypes)
+                                     ->with('productreceivetypes', $productreceivetypes)
+                                     ->with('productdistributions', $productdistributions)
+                                     ->with('adjustments', $adjustments)
+                                     ->with('reportings', $reportings)
                                      ->with('products', $products)
-                                     ->with('employeeinformations',$employeeinformations)
-                                     ->with('requisitionlists',$requisitionlists)
-                                     ->with('brands',$brands);
-     }
-     else{
-     	 return "<h2>The route you are looking for is not available.</h2>";
-     }
-
+                                     ->with('employeeinformations', $employeeinformations)
+                                     ->with('requisitionlists', $requisitionlists)
+                                     ->with('brands', $brands);
+        } else {
+            return "<h2>The route you are looking for is not available.</h2>";
+        }
     }
 
-        public function requisitionListStore(Request $request)
-
+    public function requisitionListStore(Request $request)
     {
         // dd($request->all());
         $this->validate($request, [
@@ -69,21 +66,21 @@ class requisitioninfoController extends Controller
         
 
        
-            $requisitionList=new RequisitionList;
-            $requisitionList->employee_information_id=$request->name;
-            $requisitionList->product_info_id=$request->productCode;
-            $requisitionList->quantity=$request->quantity;
-            $requisitionList->user_id=$request->session()->get('user')->id;
-            $requisitionList->requisitionDate=date('Y-m-d', strtotime($request->requisitionDate));
-            //dd($requisitionList);
-            $requisitionList->save();
+        $requisitionList=new RequisitionList;
+        $requisitionList->employee_information_id=$request->name;
+        $requisitionList->product_info_id=$request->productCode;
+        $requisitionList->quantity=$request->quantity;
+        $requisitionList->user_id=$request->session()->get('user')->id;
+        $requisitionList->requisitionDate=date('Y-m-d', strtotime($request->requisitionDate));
+        //dd($requisitionList);
+        $requisitionList->save();
 
         
-           //dd($requisitionList);
+        //dd($requisitionList);
 
         return redirect()->route('product receive.requisition info');
-    } 
-       public function deleteItemFromRequisitionList(Request $request)
+    }
+    public function deleteItemFromRequisitionList(Request $request)
     {
         if ($request->ajax()) {
             $isAvailable= RequisitionList::find($request->id);
@@ -95,24 +92,24 @@ class requisitioninfoController extends Controller
         }
     }
 
-     public function editItemFromRequisitionList(Request $request)
+    public function editItemFromRequisitionList(Request $request)
     {
         // if ($request->ajax()) {
-            $isAvailable= RequisitionList::find($request->id);
-            $employeeinformations=EmployeeInformation::all();
-            $products=ProductInfo::all();
+        $isAvailable= RequisitionList::find($request->id);
+        $employeeinformations=EmployeeInformation::all();
+        $products=ProductInfo::all();
             
-            return view('product receive.ajaxEditRequisition')
+        return view('product receive.ajaxEditRequisition')
                    ->with('employeeinformations', $employeeinformations)
                    ->with('products', $products)
                    ->with('requisitionList', $isAvailable);
         // }
     }
 
-        public function updateItemFromRequisitionList(Request $request)
+    public function updateItemFromRequisitionList(Request $request)
     {
         // return $request->all();
-          $validtor=Validator::make($request->all(), [
+        $validtor=Validator::make($request->all(), [
             'name'=>'required',
             'categoryName'=>'required',
             'productCode'=>['required', Rule::unique('product_info_id')->ignore($request->productCode)],
@@ -121,54 +118,53 @@ class requisitioninfoController extends Controller
             'requisitionDate'=>'required | date| before_or_equal:today',
             'quantity'=>'required|numeric|gt:0'
         ]);
-          if($validtor->fails()){
+        if ($validtor->fails()) {
             return ["error",$validtor->errors()];
-          }
-       $employeeinformations=EmployeeInformation::find($request->name);
-       $products=ProductInfo::find($request->productCode);
+        }
+        $employeeinformations=EmployeeInformation::find($request->name);
+        $products=ProductInfo::find($request->productCode);
         
 
        
-            $requisitionList=RequisitionList::find($request->id);
-            $requisitionList->employee_information_id=$request->name;
-            $requisitionList->product_info_id=$request->productCode;
-            $requisitionList->quantity=$request->quantity;
-            $requisitionList->user_id=$request->session()->get('user')->id;
-            $requisitionList->requisitionDate=date('Y-m-d', strtotime($request->requisitionDate));
-            //dd($requisitionList);
-            $requisitionList->save();
-            return ["success"];
-     }
+        $requisitionList=RequisitionList::find($request->id);
+        $requisitionList->employee_information_id=$request->name;
+        $requisitionList->product_info_id=$request->productCode;
+        $requisitionList->quantity=$request->quantity;
+        $requisitionList->user_id=$request->session()->get('user')->id;
+        $requisitionList->requisitionDate=date('Y-m-d', strtotime($request->requisitionDate));
+        //dd($requisitionList);
+        $requisitionList->save();
+        return ["success"];
+    }
      
 
-         public function saveAllItemFromRequisitionList(Request $request)
+    public function saveAllItemFromRequisitionList(Request $request)
     {
         //return $request->all();
         if (session()->has('user')) {
-           $requisitionlists=RequisitionList::all();
+            $requisitionlists=RequisitionList::all();
             $k=0;
             foreach ($requisitionlists as $key => $item) {
                 $saveNewRequisition=new RequisitionSave;
-                $saveNewRequisition->employee_information_id=$item->name;
-                $saveNewRequisition->product_info_id=$item->productCode;
+                $saveNewRequisition->employee_information_id=$item->employee_information_id;
+                $saveNewRequisition->product_info_id=$item->product_info_id;
                 $saveNewRequisition->quantity=$item->quantity;
                 $saveNewRequisition->user_id=$item->user_id;
                 $saveNewRequisition->requisitionDate=$item->requisitionDate;
                 $saveNewRequisition->save();
+                $findProduct=ProductInfo::find($item->product_info_id);
+                if ($findProduct) {
+                    $findProduct->stock=$findProduct->stock + $item->quantity;
+                    $findProduct->save();
+                    $item->delete();
+                }
               
                 $k++;
-
             }
-             //return $saveNewRequisition;
+            //return $saveNewRequisition;
             if (count($requisitionlists)==$k) {
                 return ["success"];
             }
-       }
+        }
     }
-
- 
-
-  
-
-
 }
