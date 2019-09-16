4b65cd8b8d2b9e236a4d7b78e2eb418fd4ac4428
Md-AshiqurRahman <53116858+Md-AshiqurRahman@users.noreply.github.com>
2019-09-16 16:30:59 +0600

    change in design

--- blri/app/Http/Controllers/productrepairController.php
+++ blri/app/Http/Controllers/productrepairController.php
@@ -7,16 +7,24 @@ use App\setuptype;
 use App\SecurityType;
 use App\ProductReceiveType;
 use App\ProductDistribution;
+use App\Adjustment;
+use App\Reporting;//model name;
+use App\User;
+use App\EmployeeInformation;
+
 
 class productrepairController extends Controller
 {
     public function index()
     {
-    $setuptypes= setuptype::all();
-    $securitytypes= SecurityType::all();
-    $productreceivetypes=ProductReceiveType::all();
-    $productdistributions=ProductDistribution::all();
+       $setuptypes= setuptype::all();
+        $securitytypes=SecurityType::all();
+        $productreceivetypes=ProductReceiveType::all();
+        $productdistributions=ProductDistribution::all();
+        $adjustments=Adjustment::all();
+        $reportings=Reporting::all();
+        $users=User::all();
 
-    return view('product distribution.product repair')->with('setuptypes',$setuptypes)->with('securitytypes',$securitytypes)->with('productreceivetypes',$productreceivetypes)->with('productdistributions',$productdistributions);
+    return view('product distribution.product repair')->with('setuptypes',$setuptypes)->with('securitytypes',$securitytypes)->with('productreceivetypes',$productreceivetypes)->with('productdistributions',$productdistributions)->with('adjustments',$adjustments)->with('reportings',$reportings)->with('users',$users);
 	}
 }
