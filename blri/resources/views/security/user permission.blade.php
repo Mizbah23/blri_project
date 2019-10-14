cee2c5445eaf92a863b81abb1dd1d5425e553448
Mizbah Uddin Ahmed <mizbahmed161@gmail.com>
2019-10-14 16:25:36 +0600

    Eng to Bangla Convert

--- blri/resources/views/security/user permission.blade.php
+++ blri/resources/views/security/user permission.blade.php
@@ -189,7 +189,7 @@ $( function() {
                 @foreach($securitytypes as $securitytype)
                    
                       <li><a href="{{route('security.'.strtolower($securitytype->SecType))}}">
-                      <i class="fa fa-circle"></i> {{$securitytype->SecType}}</a></li>
+                      <i class="fa fa-circle"></i> {{$securitytype->name}}</a></li>
                  @endforeach
                 </ul>
               </li>
